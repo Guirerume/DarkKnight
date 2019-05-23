df5cb17c6afe9962458f6769c6ff28e375b12209
Guirerume <gui.paulac@gmail.com>
2019-05-23 00:01:23 -0300

    Add Project

---
+++ Assets/scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    private Transform bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void Change(float health)
+    {
+        bar.localScale = new Vector3(health, 1f);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    public bool grounded = true;
+    public float health = .3f;
+    public int attacks = 0;
+    public int maxAttacks = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(horizontalMove);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
